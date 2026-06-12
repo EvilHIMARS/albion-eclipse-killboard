9593dc31ffb534bb5df25b36e731622c856dd219
EvilHIMARS <167474135+EvilHIMARS@users.noreply.github.com>
2026-06-12 17:08:54 +0300

    Update albion_api.py

--- albion_api.py
+++ albion_api.py
@@ -1,12 +1,15 @@
 import aiohttp
 
-BASE_URL = "https://gameinfo.albiononline.com/api/gameinfo"
+# Переключаем API на Европейский сервер (Amsterdam)
+BASE_URL = "https://gameinfo-ams.albiononline.com/api/gameinfo"
 
 async def get_events(limit=50):
     url = f"{BASE_URL}/events?limit={limit}"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            return await response.json()
+            if response.status == 200:
+                return await response.json()
+            return []
 
 async def get_guild_info(guild_id):
     url = f"{BASE_URL}/guilds/{guild_id}"
